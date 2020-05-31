892016e69448da5a5efc6c7fbf20ae19d198e73a
cris93g <37388115+cris93g@users.noreply.github.com>
2020-05-31 15:18:37 -0500

    'started'

---
+++ index.js
@@ -0,0 +1,98 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require('three');
+
+// Include any additional ThreeJS examples below
+require('three/examples/js/controls/OrbitControls');
+
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+	// Make the loop animated
+	animate: true,
+	// Get a WebGL canvas rather than 2D
+	context: 'webgl'
+};
+
+const sketch = ({ context }) => {
+	// Create a renderer
+	const renderer = new THREE.WebGLRenderer({
+		canvas: context.canvas
+	});
+
+	// WebGL background color
+	renderer.setClearColor('#000', 1);
+
+	// Setup a camera
+	const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+	camera.position.set(1, 4, -8);
+	camera.lookAt(new THREE.Vector3());
+
+	// Setup camera controller
+	const controls = new THREE.OrbitControls(camera, context.canvas);
+
+	// Setup your scene
+	const scene = new THREE.Scene();
+
+	// Setup a geometry
+	const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+	//assets come in
+	const loader = new THREE.TextureLoader();
+	const sunTexture = loader.load('./assets/sun.jpg');
+	const earthTexture = loader.load('./assets/earth.jpg');
+	const jupiterTexture = loader.load('./assets/jupiter.jpg');
+	const marsTexture = loader.load('./assets/mars.jpg');
+	const mercuryTexture = loader.load('mercury.jpg');
+	const neptuneTexture = loader.load('./assets/neptune.jpg');
+	const saturnTexture = loader.load('./assets/saturn.jpg');
+	const uranusTexture = loader.load('./assets/uranus.jpg');
+	const venusTexture = loader.load('./assets/venus.jpg');
+
+	// Setup a material
+	const material = new THREE.MeshBasicMaterial({
+		roughness: 1,
+		metalness: 0,
+		map: sunTexture
+	});
+
+	// Setup a mesh with geometry + material
+	const sunMesh = new THREE.Mesh(geometry, material);
+	scene.add(sunMesh);
+
+	const mercuryGroup = new THREE.Group();
+	const mercuryMaterial = new THREE.MeshStandardMaterial({
+		map: mercuryTexture
+	});
+	const mercuryMesh = new THREE.Mesh(geometry, mercuryMaterial);
+	mercuryMesh.position.set(2.0, 1, 0);
+	mercuryMesh.scale.setScalar(0.5);
+	mercuryGroup.add(mercuryMesh);
+	scene.add(mercuryGroup);
+	//helpers
+	scene.add(new THREE.GridHelper(5, 50));
+	let axesHelper = new THREE.AxesHelper(5);
+	scene.add(axesHelper);
+
+	// draw each frame
+	return {
+		// Handle resize events here
+		resize({ pixelRatio, viewportWidth, viewportHeight }) {
+			renderer.setPixelRatio(pixelRatio);
+			renderer.setSize(viewportWidth, viewportHeight, false);
+			camera.aspect = viewportWidth / viewportHeight;
+			camera.updateProjectionMatrix();
+		},
+		// Update & render your scene here
+		render({ time }) {
+			controls.update();
+			renderer.render(scene, camera);
+		},
+		// Dispose of events & renderer for cleaner hot-reloading
+		unload() {
+			controls.dispose();
+			renderer.dispose();
+		}
+	};
+};
+
+canvasSketch(sketch, settings);
